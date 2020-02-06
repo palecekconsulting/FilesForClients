ee5d7ba9acbadf8d9d80e932eedef8b1181867e1
palecekconsulting <43860311+palecekconsulting@users.noreply.github.com>
2020-02-06 13:35:29 -0800

    Add files via upload

---
+++ PCG GPO - BGinfo.ps1
@@ -0,0 +1,7 @@
+﻿curl  -O \\localhost\NETLOGON\bginfo.reg
+curl  -O \\localhost\NETLOGON\bginfo64.exe
+curl  -O \\localhost\NETLOGON\pcgbginfo.bgi
+curl  -O \\localhost\NETLOGON\pcgbginfo.bat
+Import-Module -Name grouppolicy
+Rename-Item -path "c:\windows\sysvol\domain\scripts\login.bat" -newname old_login.bat
+curl  -O "c:\windows\sysvol\domain\scripts\login.bat"
