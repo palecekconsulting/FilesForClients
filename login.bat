e27125fdd610beec4b161e71cb59b70f1d5326f2
palecekconsulting <43860311+palecekconsulting@users.noreply.github.com>
2020-03-08 14:57:29 -0700

    Delete login.bat

--- login.bat
+++
@@ -1,7 +0,0 @@
-%logonserver%\netlogon\bginfo64.exe %logonserver%\netlogon\pcgbginfo.bgi /TIMER:00 /nolicprompt
-
-copy %logonserver%\netlogon\pcgballlogo32.ico c:\temp /Y
-copy %logonserver%\netlogon\PCGSupport.lnk c:\temp /Y
-
-REM Remove old PCG links
-del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\bginfo.bat" /q
