d71403714b227051672f9f0a8d0e0ad3286b0ed0
palecekconsulting <43860311+palecekconsulting@users.noreply.github.com>
2020-06-23 16:48:34 -0700

    Delete pcgbginfo.bat

--- pcgbginfo.bat
+++
@@ -1,2 +0,0 @@
-reg add HKU\.DEFAULT\Software\Sysinternals\BGInfo /v EulaAccepted /t REG_DWORD /d 1 /f
-\\%logonserver%\netlogon\bginfo64.exe \\%logonserver%\netlogon\pcgbginfo.bgi /TIMER:00 /nolicprompt
